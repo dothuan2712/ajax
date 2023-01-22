6a8fb51114cfad69888998004fbcc2acc275880b
dothuan2712 <dothuan2712@gmail.com>
2023-01-22 19:34:14 +0700

    th3 html changed

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ weather[0]["icon"]
 const li = document.createElement("li");
 li.classList.add("city");
 const markup = `
-// <h2 class="city-name" data-name="${name},${sys.country}">
-// <span>${name}</span>
-// <sup>${sys.country}</sup>
-// </h2>
+<h2 class="city-name" data-name="${name},${sys.country}">
+<span>${name}</span>
+<sup>${sys.country}</sup>
+</h2>
 <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
 <figure>
 <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
@@ -42,4 +42,5 @@ msg.textContent = "Please search for a valid city!";
 msg.textContent = "";
 form.reset();
 input.focus();
-});
+})
+;
